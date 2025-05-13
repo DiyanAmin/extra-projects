22f5f9a9b8593b7944c7ef5718c473949eac7617
DiyanAmin <pakuptl3112@gmail.com>
2025-05-13 11:20:58 +0530

    added_debug-statements

--- characterchecker.py
+++ characterchecker.py
@@ -2,8 +2,14 @@
 userinput=input('Enter 1 value: ')
 print(type(userinput))
 if userinput>='a' and userinput<='z':
+    userinput=str
+    print(type(userinput))
     print('Alphabet')
-elif userinput>='1': # -> Showing Error
+elif userinput>=(1): # -> Showing Error
+    userinput=int
+    print(type(userinput))
     print('Number')
-# else:
-#     print("Special Character")
+else:
+    userinput=complex
+    print(type(userinput))
+    print("Special Character")
