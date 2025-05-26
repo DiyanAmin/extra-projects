930f12861155270af060c95a3550875f34d22701
DiyanAmin <pakuptl3112@gmail.com>
2025-05-26 16:37:27 +0530

    completed

---
+++ hcf-using-function.py
@@ -0,0 +1,11 @@
+#Calculating HCF using 'hcf' user defined function
+def hcf(n1,n2):
+    while n2>0:
+        temp=n2
+        n2=n1%n2
+        n1=temp
+    h=n1
+    print('Hcf is',h)
+n1=int(input('Enter number 1: '))
+n2=int(input('Enter number 2: '))
+hcf(n1,n2)
