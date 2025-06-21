86afb5bbbd0c4dae9175e1f2b686739a7ab3f836
DiyanAmin <pakuptl3112@gmail.com>
2025-06-21 12:02:21 +0530

    working on mirror completed isdisjoint

---
+++ isdisjoint.py
@@ -0,0 +1,7 @@
+# create 2 sets
+# use 2 sets and find the following methods in isdisjoint,difference,|
+set1={'hi','print(hello world)',1,-13,23114.412}
+set2={'...',13.231,-131,12,'Yggtradistkyx'}
+print(f'isdisjoint: {set1.isdisjoint(set2)}')
+print(f'Difference: {set1.difference(set2)}')
+print(f'| : {set1.intersection(set2)}')
---
+++ mirroring-dictionary.py
@@ -0,0 +1,15 @@
+#mirroring dictionary:-
+# create dictionary with key value
+# dict={'Name':'Diyan','Diyan':'Name'}
+# reverse value
+# for key in dict:
+    # print(f'{key}')
+# so it looks like a palindrome
+# for i in dict:
+#     if i=='Name':
+#         back=i
+#     elif i=='Diyan':
+#         front=i
+#         print(f'{back} {front}')
+# separate out str after length to change it
+# mirror string
